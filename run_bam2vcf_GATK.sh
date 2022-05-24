6f8388cbc717c75ff31870e95b858728c0e59169
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-24 18:21:45 +0200

    Update run_bam2vcf_GATK.sh

--- run_bam2vcf_GATK.sh
+++ run_bam2vcf_GATK.sh
@@ -3,7 +3,11 @@
 
 #!/bin/bash
 
-java -jar gatk-package-4.2.3.0-local.jar HaplotypeCaller -R /media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/reference/hs37d5.fa -I /media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/Jorge/HG00171.mapped.ILLUMINA.bwa.FIN.low_coverage.20120522.bam -O /media/worky_ant/Seagate_8T/Alicia_run/HG00171.g.vcf.gz 
+reference="path_to_reference_genome/hs37d5.fas"
+bam="path_to_bam_file/HG00171.mapped.ILLUMINA.bwa.FIN.low_coverage.20120522.bam"
+out_gvcf="path_to_output_gvcf/HG00171.g.vcf.gz"
+
+java -jar gatk-package-4.2.3.0-local.jar HaplotypeCaller -R $reference -I $bam -O $out_gvcf 
 
 
 
